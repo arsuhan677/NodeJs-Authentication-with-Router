2809af9096d496c8ed05ce6e6d36f7a0cd4f94f2
AR Suhan <aminurrahmansuhan@gmail.com>
2026-01-08 23:26:46 +0600

    update for cors notescontroller

---
+++ backend/src/controllers/noteController.js
@@ -0,0 +1,31 @@
+const {} = require("../models/noteModel");
+
+const createNoteHandler = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const user_id = req.user ? req.user.id : req.session.userId;
+
+    if (!title || !content) {
+      return res.status(400).json({ message: "title and content required.!" });
+    }
+    const note = await createNote({ user_id, title, content });
+    res.status(201).json({ success: true, data: note });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const getNoteHandler = async (req, res) => {
+  try {
+    const user_id = req.user ? req.user.id : req.session.userId;
+    const notes = await getNoteHandler(user_id);
+    res.json({ success: true, data: notes });
+  } catch (error) {
+    res.status(500).json({ success: true, message: error.message });
+  }
+};
+
+module.exports = {
+  createNoteHandler,
+  getNoteHandler,
+};
